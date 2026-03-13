dbb065133bc2f5845b719942a9d75905616d6bac
Kauã Vaz <kauavaz40@gmail.com>
2026-03-13 19:24:21 -0300

    integração do node

---
+++ BackEnd/rotas/index.js
@@ -0,0 +1,6 @@
+app.post("/ping", (req, res) => {
+    console.log("Keep-alive recebido:", new Date().toISOString());
+    return res.status(200).json({ ok: true});
+});//Manter back end ativo
+
+app.require("")
