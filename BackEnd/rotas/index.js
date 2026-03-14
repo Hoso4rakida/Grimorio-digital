f2e34c74dd275458f20bb393d7c50a05fe0c2b9a
Kauã Vaz <kauavaz40@gmail.com>
2026-03-14 00:33:29 -0300

    Rota de registro de usuário criada

--- BackEnd/rotas/index.js
+++ BackEnd/rotas/index.js
@@ -3,4 +3,36 @@ app.post("/ping", (req, res) => {
     return res.status(200).json({ ok: true});
 });//Manter back end ativo
 
-app.post("/register")
+app.post("/register", async (req, res) => {
+    const {nome, email, senha} = req.body;
+
+    try {
+        //verificar se já existe
+        const userExist = await pool.query(
+            "SELECT * FROM usuarios WHERE email = $1",
+            [email]
+        );
+        if (userExist.rows.length > 0) {
+            return res.status(400).json({
+                erro: "Usuário já existe"
+            });
+        }
+        //criptografar senha
+        const senhaHash = await bycrypt.hash(senha, 10);
+
+        //inserir usuário
+        const newUser = await pool.query(`INSERT INTO usuarios (nome, email, senha) Values ($1, $2, $3) returning id, nome, email`,[nome, email, senhaHash]
+        );
+        res.status(201).json({
+            mensagem: "Usuário criado",
+            usuario: newUser.rows[0]
+        });
+    } catch (err) {
+        console.log(error);
+
+        res.status(500).json({
+            erro: "Erro no servidor"
+        });
+    }
+});
+//Rota de Registro de usuário
